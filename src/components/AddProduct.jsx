b676dde84c9f390ac4fd195b8a4ec4dfa1552f18
uufukttas <uufukttas@hotmail.com>
2022-02-27 00:17:19 +0300

    Create Functionality Added.

---
+++ src/components/AddProduct.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function AddProduct() {
+  const [text, setText] = useState("");
+  const [form, setForm] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setForm([...form, text]);
+    setText("");
+  };
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="product">Product</label>
+        <input type="text" name="product" onChange={handleChange} />
+
+        <input type="submit" className="btn-submit" />
+      </form>
+    </div>
+  );
+}
+
+export default AddProduct;
